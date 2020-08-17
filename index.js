67606c37c1382e0c422a75b7c6157d5ac9156d15
Mandy Alber <mandy.alber@gmail.com>
2020-08-17 12:01:54 -0700

    added js file for shopping list updates

---
+++ index.js
@@ -0,0 +1,39 @@
+$(function () {
+    //enter items by hitting "Return" or clicking the "Add item" button
+    $('#js-shopping-list-form').on('submit', function(event){
+        event.preventDefault()
+        const newItem = $('#shopping-list-entry').val('')
+        $('.shopping-list').append(
+            `<li><span class="shopping-item">${newItem}</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`
+        )
+        $(this).trigger('reset')
+    })
+    //check and uncheck items on the list by clicking the "Check" button
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked')
+    })
+    //permanently remove items from the list by clicking the "Delete" button
+    $('.shopping-list').on('click','.shopping-item-delete', function(event){
+        $(this).closest('li').remove()
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
